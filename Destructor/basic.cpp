39cb5e2d3763f510dfb7c1ed3a680c95c48cf3ca
Janhavi Phale <janhaviphale0@gmail.com>
2026-06-26 22:14:34 +0530

    Added basic destructor program

---
+++ Destructor/basic.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class Student
+{
+public:
+    Student()
+    {
+        cout << "Constructor called" << endl;
+    }
+
+    ~Student()
+    {
+        cout << "Destructor called" << endl;
+    }
+};
+
+int main()
+{
+    Student s1;
+
+    cout << "Inside main function" << endl;
+
+    return 0;
+}
