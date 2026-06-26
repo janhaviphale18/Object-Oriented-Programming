5c8f97e6dafd7d46fa96b1d8a5fc37e61d77f245
Janhavi Phale <janhaviphale0@gmail.com>
2026-06-26 21:56:39 +0530

    Added default constructor program

---
+++ Constructor/default.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+class Student
+{
+public:
+    string name;
+    int age;
+
+    Student()
+    {
+        name = "Janhavi";
+        age = 19;
+    }
+
+    void display()
+    {
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
+    }
+};
+
+int main()
+{
+    Student s1;
+
+    s1.display();
+
+    return 0;
+}
