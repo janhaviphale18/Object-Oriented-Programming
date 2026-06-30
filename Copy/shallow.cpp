aaf6162856c14f802ca996da32ff369f5d8e416f
Janhavi Phale <janhaviphale0@gmail.com>
2026-06-30 21:41:35 +0530

    Added shallow copy program

---
+++ Copy/shallow.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class Student
+{
+public:
+    int *age;
+
+    Student(int a)
+    {
+        age = new int;
+        *age = a;
+    }
+
+    void display()
+    {
+        cout << "Age: " << *age << endl;
+    }
+};
+
+int main()
+{
+    Student s1(19);
+
+    Student s2 = s1;
+
+    *s2.age = 25;
+
+    cout << "Student 1" << endl;
+    s1.display();
+
+    cout << "Student 2" << endl;
+    s2.display();
+
+    return 0;
+}
